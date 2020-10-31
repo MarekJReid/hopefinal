bd4aaff4c544400957df8af94568a9c11d781732
Marek J. Reid <marekreid@gmail.com>
2020-10-31 13:52:00 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,71 +1,71 @@
-exports.createPages = async ({ actions: { createPage }, graphql }) => {
-  const data = await graphql(`
-    query MyQuery {
-      allCountriesInfoJson {
-        edges {
-          node {
-            country
-            institution {
-              name
-              about
-            }
-            intro
-            findings
-          }
-        }
-      }
-    }
-  `)
-  const Country = require.resolve("./src/components/pages/partners/Country.js")
+// exports.createPages = async ({ actions: { createPage }, graphql }) => {
+//   const data = await graphql(`
+//     query MyQuery {
+//       allCountriesInfoJson {
+//         edges {
+//           node {
+//             country
+//             institution {
+//               name
+//               about
+//             }
+//             intro
+//             findings
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   const Country = require.resolve("./src/components/pages/partners/Country.js")
 
-//   data.data.allCountriesInfoJson.edges.forEach(edge => {
-//     createPage({
-//       path: `/partners/${edge.node.country}/`,
-//       component: Country,
-//       context: {
-//         country: edge.node.country,
-//         intro: edge.node.intro,
-//         institutionName: edge.node.institution.name,
-//         institutionAbout: edge.node.institution.about,
-//         findings: edge.node.findings,
-//       },
-//     })
-//   })
-  const newsData = await graphql(`
-  query {
-    allContentfulNews {
-      edges {
-        node {
-          slug
-          author
-          bodyNormal {
-            bodyNormal
-          }
-          date
-          institution
-          newsTitle
-          subtitle
-        }
-      }
-    }
-  }
-  `)
-//   const Article = require.resolve("./src/components/pages/News/Article.js")
+// //   data.data.allCountriesInfoJson.edges.forEach(edge => {
+// //     createPage({
+// //       path: `/partners/${edge.node.country}/`,
+// //       component: Country,
+// //       context: {
+// //         country: edge.node.country,
+// //         intro: edge.node.intro,
+// //         institutionName: edge.node.institution.name,
+// //         institutionAbout: edge.node.institution.about,
+// //         findings: edge.node.findings,
+// //       },
+// //     })
+// //   })
+//   const newsData = await graphql(`
+//   query {
+//     allContentfulNews {
+//       edges {
+//         node {
+//           slug
+//           author
+//           bodyNormal {
+//             bodyNormal
+//           }
+//           date
+//           institution
+//           newsTitle
+//           subtitle
+//         }
+//       }
+//     }
+//   }
+//   `)
+// //   const Article = require.resolve("./src/components/pages/News/Article.js")
  
-//   newsData.data.allContentfulNews.edges.forEach(edge => {
-//     createPage({
-//       path: `/news/${edge.node.slug}/`,
-//       component: Article,
-//       context: {
-//         author: edge.node.author,
-//         title: edge.node.newsTitle,
-//         subtitle: edge.node.subtitle,
-//         date: edge.node.date,
-//         body: edge.node.bodyNormal.bodyNormal
-//       },
-//     })
-  })
-}
+// //   newsData.data.allContentfulNews.edges.forEach(edge => {
+// //     createPage({
+// //       path: `/news/${edge.node.slug}/`,
+// //       component: Article,
+// //       context: {
+// //         author: edge.node.author,
+// //         title: edge.node.newsTitle,
+// //         subtitle: edge.node.subtitle,
+// //         date: edge.node.date,
+// //         body: edge.node.bodyNormal.bodyNormal
+// //       },
+// //     })
+//   })
+// }
 
 
 
